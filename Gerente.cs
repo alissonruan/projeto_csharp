9ad0c30a426b065ea3f9ac685e5d08f96dd82a41
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-19 13:43:16 -0300

    edição Gerente

--- Gerente.cs
+++ Gerente.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ca1504
 {
-    public class Gerente : Funcionario
+    internal class Gerente : Funcionario
     {
         protected string setor;
 
-        public override void cadastrar()
-        {
-            base.cadastrar();
-            Console.WriteLine("setor: ");
+        public override void cadastrar() { 
+         base.cadastrar();
+            Console.WriteLine("Setor:");
             setor = Console.ReadLine();
         }
+
+       
     }
 }
