e4ff5077e05ee6352b3fc32e0981ffe85498f2e7
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-19 13:42:27 -0300

    edição Pessoa

--- Pessoa.cs
+++ Pessoa.cs
@@ -2,65 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ca1504
 {
-    public class Pessoa
+    internal class Pessoa
     {
         protected string nome;
-
         public string Nome
         {
             get
             {
-                return Nome.ToUpper();
-
+                return nome.ToUpper();
             }
             set
             {
                 if (value.Length > 3)
                 {
                     nome = value.ToUpper();
-                }
-                else
+                } else
                 {
-                    Console.WriteLine("Nome inválido");
+                    Console.WriteLine("nome inválido");
                 }
             }
         }
-
-
         protected string email;
-
         public string Email
         {
-            get; set;
+            get {
+                return email.ToUpper();
+            }
+            set { 
+                email = value.ToUpper();
+            }
+            
         }
-
-
+       
         public virtual void cadastrar()
         {
-            /*Console.WriteLine("Cadastre seus dados: ");
-            this.nome = Console.ReadLine();
-            this.email = Console.ReadLine();*/
-
-            Console.WriteLine("Digite o seu nome");
+            Console.WriteLine("Digite seu nome:");
             this.Nome = Console.ReadLine();
-            Console.WriteLine("Digite o seu e-mail");
+            Console.WriteLine("Digite seu e-mail:");
             this.Email = Console.ReadLine();
         }
+
         public void exibirDados()
         {
-            /*Console.WriteLine("Digite os seus dados: ");
-            this.nome = Console.ReadLine();
-            this.email = Console.ReadLine();*/
-
-            Console.WriteLine("Nome: {0}", this.Nome);
-            Console.WriteLine("Email: {0}", this.Email);
-            
+            Console.WriteLine("Nome:{0}",this.Nome);
+            Console.WriteLine("E-Mail:{0}",this.Email);
         }
     }
 }
