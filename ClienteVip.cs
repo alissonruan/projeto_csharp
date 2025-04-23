cc78336e3d12e6779cf6488589b43711fd499dd3
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-23 13:35:31 -0300

    Atividade final ClienteVip

--- ClienteVip.cs
+++ ClienteVip.cs
@@ -12,11 +12,29 @@ namespace ca1504
         protected double cashback;
 
 
-        public void calcularCashback()
+        public void calcularCashback(valorcompra: double)
         {
             Console.WriteLine("Digite o valor da Compra:");
-            this.cashback = int.Parse(Console.ReadLine();
+            this.valorcompra = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o desconto do cashback:");
+            this.desconto = double.Parse(Console.ReadLine());
+            this.cashback = (this.valorcompra * this.desconto / 100);
         }
-        public void 
+        public void exibirCashback()
+        {
+            Console.WriteLine("Cashback: {0}", this.cashback);
+            this.cashback = double.Parse(Console.ReadLine());
+        }
+
+        public void calcularDesconto()
+        {
+            Console.WriteLine("Digite o desconto que deseja calcular");
+            this.desconto = double.Parse(Console.ReadLine());
+            this.desconto = (this.valorcompra * this.desconto / 100);
+        }
+        public void exibirDesconto()
+        {
+            Console.WriteLine("Desconto: {0}",this.desconto);
+        }
     }
 }
